9daaa59d0ae05fbb8498324520491d3c2838cd85
aa-del9 <asad.bscs22seecs@seecs.edu.pk>
2024-01-24 21:05:14 +0500

    login page fixes

--- src/routes/login/LoginPage.tsx
+++ src/routes/login/LoginPage.tsx
@@ -1,9 +1,11 @@
 import LoginModal from "@/components/modals/LoginModal";
-
-const LoginPage = () => {
+interface LoginPageProps {
+  toRegister: boolean;
+}
+const LoginPage = ({ toRegister }: LoginPageProps) => {
   return (
     <div className="w-[100vw] h-[100vh] bg-[url('/assets/background.png')] bg-cover">
-      <LoginModal />
+      {toRegister ? null : <LoginModal />}
     </div>
   );
 };
